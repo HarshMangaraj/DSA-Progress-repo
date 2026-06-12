f745da7bd1376aa04bea640adbd92b2bb22ff399
HarshMangaraj <harshmangaraj723@gmail.com>
2026-06-12 19:43:06 +0530

    o(n)

---
+++ Day-1/o_of_n.py
@@ -0,0 +1,15 @@
+def find_paper(papers,name):
+    for paper in papers:
+        if paper == name:
+            return True
+    return False
+
+papers_list = ["Paper1", "Paper2", "Paper3", "Paper4", "Paper5"]
+target_paper = "Paper3"
+
+result = find_paper(papers_list, target_paper)
+
+if result:
+    print(f"{target_paper} found in the list.")
+else:
+    print(f"{target_paper} not found in the list.")
