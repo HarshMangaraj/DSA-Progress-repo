3b815aaba1d31d18102479c9787ba3518409a71b
HarshMangaraj <harshmangaraj723@gmail.com>
2026-06-17 19:48:51 +0530

    time complexity comparision

---
+++ Time Complexity/comparision.py
@@ -0,0 +1,9 @@
+#comparision values for time complexity
+
+#n represents the number of elements in the input data structure. The time complexity of an algorithm is a measure of how the running time or space requirements grow as the input size grows. or in simple words the size of the input data structure.
+
+# 1 < log log n < log n < n < n^2 < n^3 < n^4 < ...... < 2^n
+
+# 1 is the best time complexity and 2^n is the worst time complexity.
+
+# 1 is the smallest then log log n, then log n, then n, then n^2, then n^3, then n^4 and so on and 2^n is the largest time complexity.
