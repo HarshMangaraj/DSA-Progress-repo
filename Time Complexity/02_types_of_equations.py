165f2f715cdc5c878af386eda27e0ec1c4e6ab3b
HarshMangaraj <harshmangaraj723@gmail.com>
2026-06-17 19:54:31 +0530

    types of equation

---
+++ Time Complexity/02_types_of_equations.py
@@ -0,0 +1,23 @@
+# 1) constant equation : - example y = 5, y = 10, y = 1000
+
+#here y is constant and does not depend on any variable. The time complexity of this equation is O(1) which is constant time complexity.
+
+# 2) linear equation : - example y = 2x + 5, y = 3x + 10, y = 4x + 1000
+
+#here y is linear and depends on the variable x. The time complexity of this equation is O(n) which is linear time complexity.
+
+# 3) quadratic equation : - example y = x^2 + 5x + 10, y = 2x^2 + 3x + 1000, y = 4x^2 + 10x + 10000
+
+#here y is quadratic and depends on the variable x. The time complexity of this equation is O(n^2) which is quadratic time complexity.
+
+# 4) cubic equation : - example y = x^3 + 5x^2 + 10x + 100, y = 2x^3 + 3x^2 + 1000, y = 4x^3 + 10x^2 + 10000
+
+#here y is cubic and depends on the variable x. The time complexity of this equation is O(n^3) which is cubic time complexity.
+
+# 5) exponential equation : - example y = 2^x + 5x^2 + 10x + 100, y = 3^x + 3x^2 + 1000, y = 4^x + 10x^2 + 10000
+
+#here y is exponential and depends on the variable x. The time complexity of this equation is O(2^n) which is exponential time complexity.
+
+# 6) logarithmic equation : - example y = log(x) + 5x^2 + 10x + 100, y = log(x) + 3x^2 + 1000, y = log(x) + 10x^2 + 10000
+
+#here y is logarithmic and depends on the variable x. The time complexity of this equation is O(log n) which is logarithmic time complexity.
