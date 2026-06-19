c86a641a596233efbce2c9ef530752eb3fa3096b
HarshMangaraj <harshmangaraj723@gmail.com>
2026-06-19 21:14:39 +0530

    add detailed explanations for various time complexities

--- Time Complexity/03_time_commplexity_intro.py
+++ Time Complexity/03_time_commplexity_intro.py
@@ -1,2 +1,11 @@
 # if we want to comapare two softwares to decide which is better according to our requirement we can use time complexity to compare them. Time complexity is a way to represent the amount of time taken by an algorithm to run in simple words it is a way to represent the efficiency of an algorithm. The time complexity of an algorithm is usually expressed using big O notation which describes the amount of time taken by an algorithm to run as a function of the size of the input data.
 
+# while taking out time complexity we assume n value large so that we can get the dominant term and we can ignore the constant terms. For example if we have an equation y = 2n^2 + 3n + 5 then we can ignore the constant term 5 and the linear term 3n because they are not significant when n is large. So we can say that the time complexity of this equation is O(n^2) which is quadratic time complexity.
+
+# example in constant time complexity if we have an equation y = 5 then we can say that the time complexity of this equation is O(1) which is constant time complexity.
+
+# in quadratic time complexity if we have an equation y = 2n^2 + 3n + 5 then we can say that the time complexity of this equation is O(n^2) which is quadratic time complexity.
+
+# and so on for other time complexities. The time complexity of an algorithm is a measure of how the running time or space requirements grow as the input size grows. or in simple words the size of the input data structure.
+
+# in simple words we denote constant time complexity as O(1), linear time complexity as O(n), quadratic time complexity as O(n^2), cubic time complexity as O(n^3), exponential time complexity as O(2^n) and logarithmic time complexity as O(log n) but it is not always the case u will get different time complexities for different algorithms.
