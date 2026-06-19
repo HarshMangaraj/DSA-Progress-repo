3b128d39622af33313a92ff491a8f2a15faca96b
HarshMangaraj <harshmangaraj723@gmail.com>
2026-06-19 21:28:52 +0530

    add: expand explanations on time complexity analysis, emphasizing the importance of all cases

--- 04_types_of_analysis.py
+++ 04_types_of_analysis.py
@@ -7,3 +7,7 @@
 # Worst case analysis :- The worst case analysis is the scenario where the algorithm performs the maximum number of steps to complete its task. It represents the least efficient execution of the algorithm or in simple words size of the input is the largest. The worst case time complexity is denoted by O(n^2) which is quadratic time complexity we denote it using big O (O) notation.
 
 # we study about 3 types of analysis so that while finding time complexity of an algorithm we can find the best case, average case and worst case time complexity of an algorithm. but we are more concerned about worst case time complexity because it is the most important and most used time complexity in real world applications thats why we often use big O notation to denote time complexity of an algorithm.
+
+# it is not certain that u will take out time complexity of an algorithm and it only has to be a worst case time complexity but it can also have best case and average case time complexity. but we are more concerned about worst case time complexity because it is the most important and most used time complexity in real world applications thats why we often use big O notation to denote time complexity of an algorithm.
+
+# most of the time the time complexity of all the 3 types of analysis is same but it is not always the case. sometimes the time complexity of all the 3 types of analysis is different. so we have to find out the time complexity of all the 3 types of analysis to get a better understanding of the algorithm.
