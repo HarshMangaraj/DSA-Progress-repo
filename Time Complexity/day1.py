4e4059990ff87eeaf0d88ff805720737c883a0d1
HarshMangaraj <harshmangaraj723@gmail.com>
2026-06-19 21:16:52 +0530

    add: reintroduce time complexity explanations and examples across multiple files

--- Time Complexity/day1.py
+++
@@ -1,6 +0,0 @@
-marks = [88, 72 , 95, 61, 79 ]
-first_mark = marks[0]
-print(first_mark)
-
-
-# what it basically does is it takes nearly 0 time to access the first element of the list and it is called constant time complexity and it is denoted as O(1) because it takes constant time to access any element in the list regardless of the size of the list.
--- arrays/nested_traversal.py
+++
@@ -1,9 +0,0 @@
-classroom = [
-    [85, 90, 78],#r0
-    [72, 88, 91],#r1
-    [95, 60, 83] #r2
-]
-
-for row in classroom:
-    for mark in row:
-        print(mark)
