55f7fea2271d32888cea01f598703e96caf3d755
HarshMangaraj <harshmangaraj723@gmail.com>
2026-06-19 21:08:12 +0530

    fix: correct formatting and add clarity to time complexity explanations

--- Time Complexity/01_comparision.py
+++ Time Complexity/01_comparision.py
@@ -2,6 +2,8 @@
 
 #n represents the number of elements in the input data structure. The time complexity of an algorithm is a measure of how the running time or space requirements grow as the input size grows. or in simple words the size of the input data structure.
 
+#in simple words n is the size of the input
+
 # 1 < log log n < log n < n < n^2 < n^3 < n^4 < ...... < 2^n
 
 # 1 is the best time complexity and 2^n is the worst time complexity.
