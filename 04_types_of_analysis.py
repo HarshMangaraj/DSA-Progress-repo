7f8076c306ca2887580905928db054765e1fce14
HarshMangaraj <harshmangaraj723@gmail.com>
2026-06-19 21:22:49 +0530

    add: introduce types of analysis for time complexity in DSA

---
+++ 04_types_of_analysis.py
@@ -0,0 +1,7 @@
+# Types of Analysis in DSA are:-
+
+# Best case analysis :- he best case analysis is the scenario where the algorithm performs the minimum number of steps to complete its task. It represents the most efficient execution of the algorithm or in simple words size of the input is the smallest. The best case time complexity is denoted by O(1) which is constant time complexity we denote it using omega ( Ω ).
+
+# Average case analysis :- The average case analysis is the scenario where the algorithm performs on average. It represents the typical execution of the algorithm or in simple words size of the input is neither smallest nor largest. The average case time complexity is denoted by O(n) which is linear time complexity we denote it using theta (θ).
+
+# Worst case analysis :- The worst case analysis is the scenario where the algorithm performs the maximum number of steps to complete its task. It represents the least efficient execution of the algorithm or in simple words size of the input is the largest. The worst case time complexity is denoted by O(n^2) which is quadratic time complexity we denote it using big O (O).
