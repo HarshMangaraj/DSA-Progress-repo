79cb02cbacee76bd2cbe25052b2e3ef447d2f1df
HarshMangaraj <harshmangaraj723@gmail.com>
2026-06-20 20:39:49 +0530

    add: introduce quadratic time complexity examples and explanations

--- 05_time_cmp.py
+++ 05_time_cmp.py
@@ -42,3 +42,21 @@ for k in range (0, n):
 # here the time complexity of this code is O(n) which is linear time complexity because the value of y is linear and depends on the variable x which is 5 as the x value increases in an linear manner.
 
 # how because first line n = 5 takes 1 unit of time then the first loop takes n units of time because it runs n times and the print statement inside the first for loop takes n unit of time so the total is 1+n+n which is 2n + 1 then the second loop takes n units of time because it runs n times and the print statement inside the second for loop takes n unit of time so the total is 1+n+n which is 2n + 1 then the third loop takes n units of time because it runs n times and the print statement inside the third for loop takes n unit of time so the total is 1+n+n which is 2n + 1 so the total time complexity of this code is 2n + 1 + 2n + 1 + 2n + 1 which is 6n + 3 so we can say that the time complexity of this code is O(n) which is linear time complexity.
+
+
+# 3 -> QUADRATIC TIME COMPLEXITY
+
+# here the equation is y = x^2 + 5x + 10, y = 2x^2 + 3x + 1000, y = 4x^2 + 10x + 10000 here the dominating part is x^2 so the time complexity of this equation is O(n^2) which is quadratic time complexity.
+
+n = 5;
+m = 10;
+
+for i in range (0, n):
+    for j in range (0, m):
+        print("the value of i is : ", i, " and the value of j is : ", j)
+
+# here the time complexity of this code is O(n^2) which is quadratic time complexity because the value of y is quadratic and depends on the variable x which is 5 as the x value increases in an quadratic manner.
+
+# how because the first line n = 5 takes 1 unit of time then the second line m = 10 takes 1 unit of time then the first loop takes n units of time because it runs n times and then the loop inside the first loop takes m units of time because it runs m times but on an assumption we can say the both loops will run n times outside then n times inside so it is n*n which is n^2 so the total time complexity of this code is O(n^2) which is quadratic time complexity.
+
+# why n * n because the first loop runs n times and for each iteration of the first loop the second loop runs n times so it is n*n which is n^2 so the total time complexity of this code is O(n^2) which is quadratic time complexity.
