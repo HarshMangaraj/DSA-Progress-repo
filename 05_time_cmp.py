faf3bbe9276d8593885b6e0a3a4b6ae62d10fe32
HarshMangaraj <harshmangaraj723@gmail.com>
2026-06-20 20:57:52 +0530

    add: introduce logarithmic time complexity examples and explanations

--- 05_time_cmp.py
+++ 05_time_cmp.py
@@ -91,3 +91,37 @@ else:
 # here the time complexity we are gonna take is o(n^3) why because we always take the worst case time complexity of an algorithm because it is the most important and most used time complexity in real world applications thats why we often use big O notation to denote time complexity of an algorithm.
 
 # here we know that the if else statement is constant time complexity because it executes a fixed number of operations regardless of the input size. so we can ignore the if else statement and take the worst case time complexity which is O(n^3) which is cubic time complexity.
+
+# 5 -> Logorithmic time complexity
+
+# here the equation is y = log(x) + 5x^2 + 10x + 100, y = log(x) + 3x^2 + 1000, y = log(x) + 10x^2 + 10000 here the dominating part is log(x) so the time complexity of this equation is O(log n) which is logarithmic time complexity.
+
+n = 10 
+while (n > 1):
+    print("the value of n is : ", n)
+    n = n // 2
+
+# here the time complexity of this code is O(log n) which is logarithmic time complexity because the value of y is logarithmic and depends on the variable x which is 10 as the x value increases in an logarithmic manner.
+
+# how because the first line n = 10 takes 1 unit of time then the while loop takes log n units of time because it runs log n times and the print statement inside the while loop takes log n unit of time so the total is 1+log n+log n which is 2log n + 1 so we can say that the time complexity of this code is O(log n) which is logarithmic time complexity.
+
+# what is log in simple words log is the number of times we have to divide a number by 2 to get 1. for example if we have a number 8 then we have to divide it by 2 three times to get 1 so log(8) = 3. if we have a number 16 then we have to divide it by 2 four times to get 1 so log(16) = 4. if we have a number 32 then we have to divide it by 2 five times to get 1 so log(32) = 5. and so on.
+
+# for odd numbers we can take the floor value of log. for example if we have a number 7 then we have to divide it by 2 two times to get 1 so log(7) = 2. if we have a number 15 then we have to divide it by 2 three times to get 1 so log(15) = 3. if we have a number 31 then we have to divide it by 2 four times to get 1 so log(31) = 4. and so on.
+
+# we often say o(k) where k is the number of times we have to divide a number by 2 to get 1.
+
+# // is different from / because // is floor division which means it will give the quotient of the division and ignore the remainder. for example if we have a number 7 then 7//2 = 3 and 7/2 = 3.5. if we have a number 15 then 15//2 = 7 and 15/2 = 7.5. if we have a number 31 then 31//2 = 15 and 31/2 = 15.5. and so on.
+
+# in simple words if ur loops is getting divided or getting halved then it is logarithmic time complexity.
+
+# it also can be opposite or reverse like 
+n =1
+while (n < 10):
+    print("the value of n is : ", n)
+    n = n * 2
+# here the time complexity of this code is O(log n) which is logarithmic time complexity because the value of y is logarithmic and depends on the variable x which is 10 as the x value increases in an logarithmic manner.
+
+# but how because here the first line n = 1 takes 1 unit of time then the while loop takes log n units of time because it runs log n times and the print statement inside the while loop takes log n unit of time so the total is 1+log n+log n which is 2log n + 1 so we can say that the time complexity of this code is O(log n) which is logarithmic time complexity.
+
+# how logn because the first time n = 1 then second time n = 2 then third time n = 4 then fourth time n = 8 then fifth time n = 16 so it is log(16) = 4 so the while loop runs log n times so the total time complexity of this code is O(log n) which is logarithmic time complexity.
