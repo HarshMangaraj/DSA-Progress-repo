291f71f2ed70d64616be04023aee824859ad96bc
HarshMangaraj <harshmangaraj723@gmail.com>
2026-06-20 20:20:12 +0530

    add: implement basic example demonstrating constant time complexity O(1)

---
+++ 05_time_cmp.py
@@ -0,0 +1,8 @@
+x = 5 ;
+print("the value of x is : ", x)
+
+#here the time complexity of this code is O(1) which is constant time complexity because the value of x is constant and does not depend on any variable.
+
+# why is it constant it is because we are not takiing any input from user and not using any for loop or while loop or any other loop to take input from user and the value of x is constant and does not depend on any variable.
+
+#if else statement is also constant time complexity because it executes a fixed number of operations regardless of the input size.
