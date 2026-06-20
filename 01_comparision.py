b434360a03aa810dca626c16fc98a05f38ff3b15
HarshMangaraj <harshmangaraj723@gmail.com>
2026-06-20 21:15:19 +0530

    add: expand explanation of exponential time complexity with detailed examples

--- 01_comparision.py
+++ 01_comparision.py
@@ -9,3 +9,19 @@
 # 1 is the best time complexity and 2^n is the worst time complexity.
 
 # 1 is the smallest then log log n, then log n, then n, then n^2, then n^3, then n^4 and so on and 2^n is the largest time complexity.
+
+
+
+
+
+# 6 -> exponential time complexity
+
+
+
+# here in exponential time complexity the time taken by the algorithm to run increases exponentially with the increase in the size of the input data structure. The time complexity of this equation is O(2^n) which is exponential time complexity.
+
+# equation y = 2^n + 5n^2 + 10n + 100, y = 3^n + 3n^2 + 1000, y = 4^n + 10n^2 + 10000
+
+# here the dominant term is 2^n, 3^n, 4^n and we can ignore the constant terms and the linear terms because they are not significant when n is large. So we can say that the time complexity of this equation is O(2^n) which is exponential time complexity.
+
+# with this time complexity is done u can find out the time complexity of an algorithm and compare it with other algorithms to find out which algorithm is better according to your requirement.
