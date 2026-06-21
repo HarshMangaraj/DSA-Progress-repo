9eae50013dd8a31c190f0a3b0d2bdb0b16055031
HarshMangaraj <harshmangaraj723@gmail.com>
2026-06-21 21:20:51 +0530

    add: improve arrays introduction with additional explanation on dynamic memory management

--- 06_arrays_intro.py
+++ 06_arrays_intro.py
@@ -5,3 +5,5 @@
 # in other languages we can directly declare or use arrays but in python is not like this if we want to use array then there are two ways:
 
 # 1> use python array module 2> Python numpy module u can use any one of this but the better option is numpy module as it has more functionality
+
+# In python arrays are dynamic in default so we dont have to do separate memory management for it like in other languages
