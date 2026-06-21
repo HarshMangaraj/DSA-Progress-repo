e5e7ea17bf5e11c6000411ce34b9616748dd9628
HarshMangaraj <harshmangaraj723@gmail.com>
2026-06-21 21:24:42 +0530

    docs: add explanation of array indexing and element access to 06_arrays_intro.py

--- 06_arrays_intro.py
+++ 06_arrays_intro.py
@@ -7,3 +7,5 @@
 # 1> use python array module 2> Python numpy module u can use any one of this but the better option is numpy module as it has more functionality
 
 # In python arrays are dynamic in default so we dont have to do separate memory management for it like in other languages
+
+# arrays index start from 0 to n-1 so if we want to create an array of 5 elements then the indexes will be 0 1 2 3 4 and also we can access each index using example a[1] or arr[2] etc
