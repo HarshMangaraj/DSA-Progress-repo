8f59529a2105d912a9807f1caba4ec028e838a8c
HarshMangaraj <harshmangaraj723@gmail.com>
2026-06-21 21:18:25 +0530

    add: expand introduction to arrays with detailed explanations

--- 06_arrays_intro.py
+++ 06_arrays_intro.py
@@ -1,1 +1,7 @@
-# Arrays 
+# Arrays are storing data in contigeous manner
+
+# in other languages the array should hold similar types of data but in python we can hold different types of datas together also
+
+# in other languages we can directly declare or use arrays but in python is not like this if we want to use array then there are two ways:
+
+# 1> use python array module 2> Python numpy module u can use any one of this but the better option is numpy module as it has more functionality
